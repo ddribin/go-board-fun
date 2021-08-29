c1b252d5ea6c4c952b049df60a5990716c6c7b75
Dave Dribin <dave-vcs@dribin.org>
2021-08-29 13:36:22 -0500

    pwm: Add PWM sequencer to set the PWM parameters

---
+++ pwm/pwm_sequencer.v
@@ -0,0 +1,25 @@
+`default_nettype none
+
+module pwm_sequencer (
+  input wire        i_clk,
+
+  output wire [7:0] o_top,
+  output wire       o_top_valid,
+  output wire [8:0] o_compare,
+  output wire       o_compare_valid
+);
+  
+  reg [3:0]   r_count = 0;
+  wire w_valid = r_count == 4;
+  assign o_top_valid = w_valid;
+  assign o_top = 8'hFF;
+  assign o_compare_valid = w_valid;
+  assign o_compare = 9'h7F;
+
+  always @(posedge i_clk) begin
+    if (r_count[3] == 0) begin
+      r_count <= r_count + 1;
+    end
+  end
+
+endmodule
--- pwm/pwm_top.v
+++ pwm/pwm_top.v
@@ -1,4 +1,37 @@
+`default_nettype none
+
 module pwm_top (
+  input wire  i_Clk,
+
+  output wire io_PMOD_1,
+  output wire io_PMOD_2,
+  output wire o_LED_1
 );
   
+  wire [7:0]  w_top;
+  wire        w_top_valid;
+  wire [8:0]  w_compare;
+  wire        w_compare_valid;
+  pwm_sequencer pwm_sequencer(
+    .i_clk(i_Clk),
+    .o_top(w_top),
+    .o_top_valid(w_top_valid),
+    .o_compare(w_compare),
+    .o_compare_valid(w_compare_valid)
+  );
+
+  wire w_pwm;
+  pwm pwm(
+    .i_clk(i_Clk),
+    .i_top(w_top),
+    .i_top_valid(w_top_valid),
+    .i_compare(w_compare),
+    .i_compare_valid(w_compare_valid),
+    .o_pwm(w_pwm)
+  );
+
+  assign io_PMOD_1 = w_pwm;
+  assign io_PMOD_2 = ~w_pwm;
+  assign o_LED_1 = w_pwm;
+
 endmodule

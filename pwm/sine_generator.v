c021c844bdb1df39bd83aa3e5c4eedd9bdf9ff48
Dave Dribin <dave-vcs@dribin.org>
2021-08-31 16:04:35 -0500

    pwm: Use a 64-entry sine table and calculate the other values based on the sine wave's symmetry

--- pwm/sine_generator.v
+++ pwm/sine_generator.v
@@ -5,11 +5,18 @@ module sine_generator (
   output wire [8:0]   o_compare
 );
   
-  reg [8:0] sine_table[255:0];
+  reg [8:0] sine_table[63:0];
   initial begin
     $readmemh("sine_table.txt", sine_table);
   end
 
-  wire [7:0]  w_sine_lookup = i_phase[31:24];
-  assign o_compare = sine_table[w_sine_lookup] >> 2;
+  wire [1:0]  w_quadrant = i_phase[31:30];
+  wire [5:0]  w_index = i_phase[29:24];
+  wire [5:0]  w_adjusted_index = w_quadrant[0]? ~w_index : w_index;
+
+  wire [8:0]  w_output = sine_table[w_adjusted_index];
+  wire [8:0]  w_output_reflected = (w_output ^ 9'hFF) + 9'd1;
+  wire [8:0]  w_adjusted_output = w_quadrant[1]? w_output_reflected : w_output;
+
+  assign o_compare = w_adjusted_output >> 2;
 endmodule
--- pwm/sine_table.rb
+++ pwm/sine_table.rb
@@ -1,9 +1,11 @@
 #!/usr/bin/env ruby
 
-(0..255).each do |i|
+(0..63).each_with_index do |i, index|
   radians = i*2*Math::PI/256
   sin_0_to_2_float = Math.sin(radians) + 1.0
   sin_0_to_256_float = 128 * sin_0_to_2_float
   sin_0_to_256_int = sin_0_to_256_float.round
   printf "%03X\n", sin_0_to_256_int
+  # reflect = ((sin_0_to_256_int ^ 0xFF) + 1) & 0x1FF
+  # printf "%3d: %03X %3d -> %03X %3d\n", index, sin_0_to_256_int, sin_0_to_256_int, reflect, reflect
 end

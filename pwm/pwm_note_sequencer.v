6ec3a1d0dc37464c0b3cada6d0c4ee45f29e3ad1
Dave Dribin <dave-vcs@dribin.org>
2021-09-03 23:04:29 -0500

    pwm: Extract note length table to its own module, note_length_table.v

--- pwm/pwm_note_sequencer.v
+++ pwm/pwm_note_sequencer.v
@@ -9,11 +9,6 @@ module pwm_note_sequencer (
   output wire [31:0]   o_phase_delta
 );
 
-  // localparam DURATION = 25_000_000/4;
-  // localparam DURATION = 6_250_000;
-  localparam DURATION = 4_166_667; // 180 BPM: DURATION = 25MHz * 30 / 180
-
-
   function [4:0] note_len;
     input [4:0] i_len;
     begin
@@ -21,66 +16,24 @@ module pwm_note_sequencer (
     end
   endfunction
   
-  localparam LEN_1 = note_len(1);
-  localparam LEN_2 = note_len(2);
-  localparam LEN_3 = note_len(3);
-  localparam LEN_4 = 5'd3;
-  localparam LEN_6 = 5'd5;
-  localparam LEN_7 = 32'd14_583_333;
-  localparam LEN_9 = 32'd18_750_000;
-  localparam LEN_15 = 32'd31_250_000;
+  `define LEN_THIRTY_SECOND note_len(1)
+  `define NOTE_LEN_SIXTEENTH note_len(2)
+  `define NOTE_LEN_EIGHTH note_len(4)
+  `define NOTE_LEN_QUARTER note_len(8)
+  `define NOTE_LEN_HALF note_len(16)
+  `define NOTE_LEN_WHOLE note_len(32)
 
-  localparam LEN_THIRTY_SECOND = LEN_1;
-  localparam LEN_SIXTEENTH = LEN_2;
-  localparam LEN_EIGHTH = LEN_4;
-  localparam LEN_QUARTER = 32'd16_666_667;
-  localparam LEN_HALF = 32'd33_333_333;
-  localparam LEN_WHOLE = 32'd66_666_667;
-
-  reg [31:0]  r_duration = 0;
-  reg [4:0]   r_note_len = 0;
-  always @(*) begin
-    case (r_note_len)
-      5'd00: r_duration = 32'd2083333;
-      5'd01: r_duration = 32'd4166667;
-      5'd02: r_duration = 32'd6250000;
-      5'd03: r_duration = 32'd8333333;
-      5'd04: r_duration = 32'd10416667;
-      5'd05: r_duration = 32'd12500000;
-      5'd06: r_duration = 32'd14583333;
-      5'd07: r_duration = 32'd16666667;
-      5'd08: r_duration = 32'd18750000;
-      5'd09: r_duration = 32'd20833333;
-      5'd10: r_duration = 32'd22916667;
-      5'd11: r_duration = 32'd25000000;
-      5'd12: r_duration = 32'd27083333;
-      5'd13: r_duration = 32'd29166667;
-      5'd14: r_duration = 32'd31250000;
-      5'd15: r_duration = 32'd33333333;
-      5'd16: r_duration = 32'd35416667;
-      5'd17: r_duration = 32'd37500000;
-      5'd18: r_duration = 32'd39583333;
-      5'd19: r_duration = 32'd41666667;
-      5'd20: r_duration = 32'd43750000;
-      5'd21: r_duration = 32'd45833333;
-      5'd22: r_duration = 32'd47916667;
-      5'd23: r_duration = 32'd50000000;
-      5'd24: r_duration = 32'd52083333;
-      5'd25: r_duration = 32'd54166667;
-      5'd26: r_duration = 32'd56250000;
-      5'd27: r_duration = 32'd58333333;
-      5'd28: r_duration = 32'd60416667;
-      5'd29: r_duration = 32'd62500000;
-      5'd30: r_duration = 32'd64583333;
-      5'd31: r_duration = 32'd66666667;
-    endcase
-  end
+  reg   [4:0]   r_note_len = 0;
+  wire  [31:0]  r_note_duration;
+  note_length_table length_table(
+    .i_note_len(r_note_len),
+    .o_duration(r_note_duration)
+  );
 
   reg [31:0] r_duration_count = 0;
   reg [3:0] r_note_index = 0;
 
   reg [5:0]  r_note = 0;
-  wire [31:0] r_note_duration = r_duration;
   wire r_note_done = r_duration_count == r_note_duration;
 
   always @(posedge i_clk) begin
@@ -132,20 +85,9 @@ module pwm_note_sequencer (
       4'd10: begin r_note = `NOTE_Fs4; r_note_len = note_len(12); end
       4'd11: begin r_note = `NOTE_Fs5; r_note_len = note_len(30); end
       4'd12: begin r_note = `NOTE_Gs5; r_note_len = note_len(4); end
-      4'd13: begin r_note = `NOTE_Fs5; r_note_len = note_len(1); end
+      4'd13: begin r_note = `NOTE_Fs5; r_note_len = note_len(2); end
       4'd14: begin r_note = `NOTE_Cs6; r_note_len = note_len(30); end
       4'd15: begin r_note = `NOTE_RST; r_note_len = note_len(4); end
-  //     4'd06: begin r_note = `NOTE_Gs5; r_note_duration = DURATION; end
-  //     4'd07: begin r_note = `NOTE_B5;  r_note_duration = DURATION; end
-  //     4'd08: begin r_note = `NOTE_Cs5; r_note_duration = DURATION; end
-  //     4'd09: begin r_note = `NOTE_B5;  r_note_duration = DURATION; end
-  //     4'd10: begin r_note = `NOTE_As5; r_note_duration = DURATION; end
-  //     4'd11: begin r_note = `NOTE_Cs5; r_note_duration = DURATION; end
-  //     4'd12: begin r_note = `NOTE_As5; r_note_duration = DURATION; end
-  //     4'd13: begin r_note = `NOTE_Gs5; r_note_duration = DURATION; end
-  //     4'd14: begin r_note = `NOTE_Fs5; r_note_duration = DURATION; end
-  //     4'd15: begin r_note = `NOTE_RST; r_note_duration = DURATION; end
-      
       default: begin r_note = `NOTE_RST;   r_note_len = note_len(4); end
     endcase
   end
